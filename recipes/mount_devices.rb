2fbd5df03608b8a39bf34e17e6e8d3a42f9d5ba2
Kannan Manickam <me@arangamani.net>
2013-08-15 01:23:59 -0700

    updated the mount_devices recipe - not tested yet

--- recipes/mount_devices.rb
+++ recipes/mount_devices.rb
@@ -16,3 +16,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+devices = node['media']['devices'].keys
+
+devices.each do |mnt_device|
+  # Obtain the mount point for the device from the devices Hash
+  mount_point = node['media']['devices'][mnt_device]
+
+  # Create the mount point directory
+  directory mount_point do
+    recursive true
+    owner node['media']['user']
+    group node['media']['group']
+    mode 0755
+    action :create
+  end
+
+  # Obtain the file system type using the helper. Refer to libraries/helper.rb
+  # for the definition of the `get_file_system_type` method.
+  file_system_type = Media::Helper.get_file_system_type(mnt_device)
+  raise "Unable to find the file system type for device '#{mnt_device}'" \
+    if file_system_type.nil?
+
+  # Mount the device in the specified
+  mount mount_point do
+    device mnt_device
+    fstype file_system_type
+    action [:create, :enable]
+  end
+end
