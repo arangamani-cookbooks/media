678ca63160716bb830342dae6c7d148f1912f7f8
Kannan Manickam <me@arangamani.net>
2013-08-15 00:17:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# media cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Kannan Manickam (<me@arangamani.net>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'media'
+maintainer       'Kannan Manickam'
+maintainer_email 'me@arangamani.net'
+license          'Apache 2.0'
+description      'Installs/Configures media'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
